0db68a4b774213cff1e75f2a3f3c158f545efbe7
juber2023 <juber.eco@gmail.com>
2023-06-09 10:35:19 +0600

    dashboard structure

--- src/Pages/AdminDashBoard.jsx
+++ src/Pages/AdminDashBoard.jsx
@@ -1,11 +1,44 @@
-import React from 'react';
+import { useState } from 'react';
 
-const AdminDashBoard = () => {
-    return (
-        <div>
-            admin
-        </div>
-    );
+const AdminDashboard = () => {
+  const [activeItem, setActiveItem] = useState('item1');
+
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-1/4 bg-gray-200 min-h-[calc(100vh-305px)] ">
+        <ul className="py-20 ">
+          <li
+            className={`px-4 py-2 cursor-pointer ${
+              activeItem === 'item1' ? 'bg-lime-500 text-white' : ''
+            }`}
+            onClick={() => handleItemClick('item1')}
+          >
+            Manage Classes
+          </li>
+          <li
+            className={`px-4 py-2 cursor-pointer ${
+              activeItem === 'item2' ? 'bg-lime-500 text-white' : ''
+            }`}
+            onClick={() => handleItemClick('item2')}
+          >
+            Manage Users
+          </li>
+          
+          
+        </ul>
+      </div>
+      <div className="w-3/4 bg-white p-4">
+        
+        {activeItem === 'item1' && <h1>Content for Item 1</h1>}
+        {activeItem === 'item2' && <h1>Content for Item 2</h1>}
+       
+      </div>
+    </div>
+  );
 };
 
-export default AdminDashBoard;
+export default AdminDashboard;
--- src/Pages/InstructorDashboard.jsx
+++ src/Pages/InstructorDashboard.jsx
@@ -1,11 +1,52 @@
 
+import { useState } from 'react';
 
 const InstructorDashboard = () => {
-    return (
-        <div>
-            instructor
-        </div>
-    );
+  const [activeItem, setActiveItem] = useState('item1');
+
+  const handleItemClick = (itemName) => {
+    setActiveItem(itemName);
+  };
+
+  return (
+    <div className="flex">
+      <div className="w-1/4 bg-gray-200 min-h-[calc(100vh-305px)] ">
+        <ul className="py-20 ">
+          <li
+            className={`px-4 py-2 cursor-pointer ${
+              activeItem === 'item1' ? 'bg-lime-500 text-white' : ''
+            }`}
+            onClick={() => handleItemClick('item1')}
+          >
+            Item 1
+          </li>
+          <li
+            className={`px-4 py-2 cursor-pointer ${
+              activeItem === 'item2' ? 'bg-lime-500 text-white' : ''
+            }`}
+            onClick={() => handleItemClick('item2')}
+          >
+            Item 2
+          </li>
+          <li
+            className={`px-4 py-2 cursor-pointer ${
+              activeItem === 'item3' ? 'bg-lime-500 text-white' : ''
+            }`}
+            onClick={() => handleItemClick('item3')}
+          >
+            Item 3
+          </li>
+          
+        </ul>
+      </div>
+      <div className="w-3/4 bg-white p-4">
+        
+        {activeItem === 'item1' && <h1>Content for Item 1</h1>}
+        {activeItem === 'item2' && <h1>Content for Item 2</h1>}
+        {activeItem === 'item3' && <h1>Content for Item 3</h1>}
+      </div>
+    </div>
+  );
 };
 
 export default InstructorDashboard;
