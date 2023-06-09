b8b27fab8936d6a51edac647118386cca06c815b
juber2023 <juber.eco@gmail.com>
2023-06-09 07:36:26 +0600

    marquee

--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,4 +1,5 @@
 import Banner from './Banner';
+import MarqueeFast from './Marquee';
 import PopularClasses from './PopularClasses';
 import PopularInstructors from './PopularInstructors';
 
@@ -6,6 +7,7 @@ const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <MarqueeFast></MarqueeFast>
             <PopularClasses></PopularClasses>
             <PopularInstructors></PopularInstructors>
         </div>
---
+++ src/Home/Marquee.jsx
@@ -0,0 +1,13 @@
+import Marquee from 'react-fast-marquee';
+
+const MarqueeFast = () => {
+    return (
+        <div>
+            
+            
+            <Marquee pauseOnHover className='text-xl font-semibold '>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;Join our exciting summer camp and immerse yourself in a world of language learning! Our foreign language training school offers a unique opportunity to enhance your linguistic skills while enjoying a fun-filled summer experience.With a wide range of facilities available, including state-of-the-art classrooms, language labs, and dedicated study areas, you'll have the perfect environment to master a new language. Our experienced instructors provide engaging lessons, interactive activities, and cultural workshops to ensure a comprehensive learning experience. From beginner to advanced levels, our program caters to all language enthusiasts. Don't miss out on this incredible opportunity to broaden your horizons and make lifelong memories. Enroll today! </Marquee>
+        </div>
+    );
+};
+
+export default MarqueeFast;
